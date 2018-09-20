b17323e6646fb86aacfc5c97d788a1465c005651
ksehyun <201403876@o.cnu.ac.kr>
2018-09-20 11:07:54 +0900

    update other functions

--- src/Calculate.java
+++ src/Calculate.java
@@ -36,17 +36,17 @@ public class Calculate {
 
 	public static int sub(int x, int y) {
 
-		return 0;
+		return x-y;
 	}
 
 	public static int mul(int x, int y) {
 
-		return 0;
+		return x*y;
 	}
 
 	public static double div(int x, int y) {
 		// 실수로 반환이 되도록 구현
 
-		return 0;
+		return x/y;
 	}
 }
